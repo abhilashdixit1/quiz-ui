4e5192616ca8dfbba50bea5517b7748d0967231e
Nikitha Ramu <nkta.1992@gmail.com>
2020-08-16 07:48:23 +0530

    Updates

---
+++ precache-manifest.33a684765dd9aaf3b300cb6be04fd203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761abb840c5d6377fcd99a6ab16e15cb",
+    "url": "/quiz-ui/index.html"
+  },
+  {
+    "revision": "861ada1eab10f6966d38",
+    "url": "/quiz-ui/static/css/main.095bf2f9.chunk.css"
+  },
+  {
+    "revision": "499dd5919112591085b2",
+    "url": "/quiz-ui/static/js/2.29596f8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-ui/static/js/2.29596f8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "861ada1eab10f6966d38",
+    "url": "/quiz-ui/static/js/main.e7c8de11.chunk.js"
+  },
+  {
+    "revision": "bef378f791a359d9fddd",
+    "url": "/quiz-ui/static/js/runtime-main.ba7feb2e.js"
+  }
+]);
